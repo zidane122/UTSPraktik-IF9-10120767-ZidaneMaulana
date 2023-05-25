3bdbfc2459b8aba2d7b96f0e1f48ff1b2c673d1b
zidane122 <45733072+zidane122@users.noreply.github.com>
2023-05-25 10:22:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if9_10120767_zidanemaulana/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_if9_10120767_zidanemaulana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
